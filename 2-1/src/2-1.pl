103b15fc091c1715980eca2cca3b3c70bd016d3e
aslogd <gerarddelcastillo@gmail.com>
2019-12-03 01:12:58 +0100

    split

--- 2-1/src/2-1.pl
+++ 2-1/src/2-1.pl
@@ -1,6 +1,20 @@
 (defun split-string (input separator)
-	(let ((result ()))
-		(loop )
+	(loop
+		for x from 0 below (length input)
+		with begin = 0
+		with end = (length input)
+		with res = ()
+		do (progn 
+			(if (string= (char input x) ",")
+				(progn 
+					(setq end x)
+					(setq res (cons (parse-integer (subseq input begin end)) res))
+					(setq begin (+ 1 x))
+					(setq end (length input))
+				)
+			)
+		)
+		finally (return (setq res (cons (parse-integer (subseq input begin end)) res)))
 	)
 )
 
@@ -10,4 +24,6 @@
 	)
 )
 
-(print (read-program))
+;;(print (read-program))
+
+(split-string "1,2,3,4,1204,1,42" ",")
